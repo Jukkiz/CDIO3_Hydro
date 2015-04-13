4e9d8166c8c30ff8a2a0551ab4aefc455f49dbfe
Jukkiz <jukki_z@hotmail.com>
2015-04-13 20:43:48 +0300

    hakua

--- php/Tilaukset.php
+++ php/Tilaukset.php
@@ -3,15 +3,26 @@ require_once("db.inc");
 
 if(isset($_GET['item']))
 {
-	$item = $_GET['item'];
+	$item  = '%'.$_GET['item'].'%';
 }
 else
 {
-	$item = "";
+	$item = "%%";
 }
 
-$result = $conn->prepare("SELECT WorkNumber, Info, Status1 FROM workcard WHERE ItemCode LIKE :ic");
-$result->execute(array(':ic' => '%'.$item.'%'));
+if(isset($_GET['itemPage']))
+{
+	$itemPage = $_GET['itemPage'];
+}
+else
+{
+	$itemPage = 0;
+}
+
+$result = $conn->prepare("SELECT WorkNumber, Info, Status1 FROM workcard WHERE ItemCode LIKE :ic LIMIT :page, 10 ");
+$result->bindParam(':page', $itemPage, PDO::PARAM_INT);
+$result->bindParam(':ic', $item, PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 20);
+$result->execute();
 $arr = array();
 
 while($r = $result->fetch(PDO::FETCH_ASSOC))
